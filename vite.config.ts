4a27eb0d2d9920dba19a95fa54a697192077ad16
jfkeci <ja.lip132@gmail.com>
2026-02-17 09:30:27 +0100

    feature: gh setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/PennyTrail/',
+  base: '/penny-trail/',
   plugins: [react(), tailwindcss()],
 })
